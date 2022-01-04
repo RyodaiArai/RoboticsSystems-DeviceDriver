36e018918a22341623bc009c51339ad0f0e6fa2d
Ryodai Arai <s20c1005ds@s.chibakoudai.jp>
2022-01-05 02:41:17 +0900

    Update

--- control.c
+++ control.c
@@ -23,64 +23,72 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 
 	if(c == '0')
 	{
-		//gpio_base[10] = 1 << 25;
+		gpio_base[10] = 1 << 25;
 		gpio_base[10] = 1 << 24;
 		//gpio_base[10] = 1 << 23;
-		gpio_base[10] = 1 << 22;
+		//gpio_base[10] = 1 << 22;
 	}
 	else if(c == '1')
 	{
-		//gpio_base[7] = 1 << 25;
+		gpio_base[10] = 1 << 25;
 		gpio_base[7] = 1 << 24;
 		//gpio_base[7] = 1 << 23;
-		gpio_base[7] = 1 << 22;
+		//gpio_base[7] = 1 << 22;
+	}
+	else if(c == '2')
+	{
+		gpio_base[10] = 1 << 24;
+		gpio_base[7] = 1 << 25;
 	}
 	return 1;
 }
 
-static struct file_operations led_fops = {
+static struct file_operations control_fops = {
 	.owner = THIS_MODULE,
 	.write = led_write
 };
-
 static int __init init_mod(void)
 {
 	int retval;
+	retval = alloc_chrdev_region(&dev, 0, 1, "control");
 
-	gpio_base = ioremap_nocache(0xfe200000, 0xA0);
-
-	const u32 led = 25;
-	const u32 index = led/10;
-	const u32 shift = (led%10)*3;
-	const u32 mask = ~(0x7 << shift);
-	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
-
-
-	retval =  alloc_chrdev_region(&dev, 0, 1, "myled");
 	if(retval < 0)
 	{
 		printk(KERN_ERR "alloc_chrdev_region failed.\n");
 		return retval;
 	}
 	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
-	
-	cdev_init(&cdv, &led_fops);
-	cdv.owner = THIS_MODULE;
+
+	cdev_init(&cdv, &control_fops);
 	retval = cdev_add(&cdv, dev, 1);
+
 	if(retval < 0)
 	{
 		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
 		return retval;
 	}
 
-	cls = class_create(THIS_MODULE,"myled");
+	cls = class_create(THIS_MODULE, "control");
+
 	if(IS_ERR(cls))
 	{
 		printk(KERN_ERR "class_create failed.");
 		return PTR_ERR(cls);
 	}
-	device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
 
+	device_create(cls, NULL, dev, NULL, "control%d", MINOR(dev));
+	gpio_base = ioremap_nocache(0xfe200000, 0xA0);
+						        
+	int i;
+
+	for(i = 0; i < 2; i++)
+	{
+		const u32 motor = usegpio[i];
+		const u32 index = motor/10;
+		const u32 shift = (motor%10)*3;
+		const u32 mask = ~(0x7 << shift);
+		gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+	}
 	return 0;
 }
 
@@ -91,7 +99,6 @@ static void __exit cleanup_mod(void)
 	class_destroy(cls);
 	unregister_chrdev_region(dev, 1);
 	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
-	iounmap(gpio_base);
 }
 
 module_init(init_mod);
